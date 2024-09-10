ea7d785739b9e3da6d3a25eba366d7f3f6f00710
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2024-09-10 12:48:03 +0200

    Select HTML Elements
    Select Necessary HTML Elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const resetButton = document.getElementsByClassName('btn__reset');
+let missed = 0;
